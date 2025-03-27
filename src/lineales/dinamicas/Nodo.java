1b20a5f57b1bb6e1bea3aef488db1dc8754f3250
GabrielFierro <fierrogabriel13@gmail.com>
2025-03-27 13:00:09 -0300

    add Nodo class for dinamics classes

--- src/lineales/dinamicas/Nodo.java
+++ src/lineales/dinamicas/Nodo.java
@@ -4,5 +4,24 @@ public class Nodo {
     private Object elem;
     private Nodo enlace;
 
+    public Nodo(Object elem, Nodo enlace){
+        this.elem = elem;
+        this.enlace = enlace;
+    }
 
+    public void setElem(Object elem){
+        this.elem = elem;
+    }
+
+    public void setEnlace(Nodo enlace){
+        this.enlace = enlace;
+    }
+
+    public Object getElem(){
+        return this.elem;
+    }
+
+    public Nodo getEnlace(){
+        return this.enlace;
+    }
 }
