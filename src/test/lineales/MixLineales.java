904492ad96c70672b6faa11ccd686b9f3f9098ee
GabrielFierro <fierrogabriel13@gmail.com>
2025-04-24 12:00:46 -0300

    fix imports and add new method like generarOtraCola

--- src/test/lineales/MixLineales.java
+++ src/test/lineales/MixLineales.java
@@ -1,14 +1,13 @@
 package test.lineales;
 
-import lineales.dinamicas.Cola;
-import lineales.dinamicas.Pila;
-import lineales.dinamicas.Nodo;
+import lineales.dinamicas.*;
 
 public class MixLineales {
 
     public static void main(String[] args){
         Cola c1 = new Cola();
         Cola nuevaCola = new Cola();
+        Cola colaAuxiliar = new Cola();
 
         System.out.println("Agregar el valor A: " + c1.poner("A"));
         System.out.println("Agregar el valor B: " + c1.poner("B"));
@@ -26,7 +25,10 @@ public class MixLineales {
         nuevaCola = generarOtraCola(c1);
 
         System.out.println("Muestro la nueva cola generada: " + nuevaCola.toString());
-
+        
+        colaAuxiliar = generar(c1);
+        
+        System.out.println("Muestro la nueva cola generada: " + colaAuxiliar.toString());
     }
 
     public static Cola generarOtraCola(Cola c1){
@@ -71,6 +73,64 @@ public class MixLineales {
         while(!aux.esVacia()){
             nuevaCola.poner(aux.obtenerTope());
             aux.desapilar();
+        }
+        return nuevaCola;
+    }
+    
+    public static Cola generar(Cola c1){
+        // $
+        // c1 = AB#C#DEF
+        // Debe retornar ABBAAB#CCC#DEFFEDDEF
+
+        // Cola: A,B -> AB
+        // pila: A,B -> BA
+        // Cola: A,B -> AB
+        // -> ABBAAB
+        // Zona de declaracion de variables
+        Cola copia, colaAux, colaAuxiliar, nuevaCola;
+        Pila aux = new Pila();
+        Object frente;
+        // Zona de inicializacion de variables
+        copia = c1.clone();
+        colaAux = new Cola();
+        colaAuxiliar = new Cola();
+        nuevaCola = new Cola();
+
+        while(!copia.esVacia()){
+            frente = copia.obtenerFrente();
+
+            if(!frente.equals("$")) {
+                aux.apilar(frente);
+                colaAux.poner(frente);
+                colaAuxiliar.poner(frente);
+            }else{
+                while(!colaAux.esVacia()){
+                    nuevaCola.poner(colaAux.obtenerFrente());
+                    colaAux.sacar();
+                }
+                while(!aux.esVacia()){
+                    nuevaCola.poner(aux.obtenerTope());
+                    aux.desapilar();
+                }
+                while(!colaAuxiliar.esVacia()){
+                    nuevaCola.poner(colaAuxiliar.obtenerFrente());
+                    colaAuxiliar.sacar();
+                }
+                nuevaCola.poner('$');
+            }
+            copia.sacar();
+        }
+        while(!colaAux.esVacia()){
+            nuevaCola.poner(colaAux.obtenerFrente());
+            colaAux.sacar();
+        }
+        while(!aux.esVacia()){
+            nuevaCola.poner(aux.obtenerTope());
+            aux.desapilar();
+        }
+        while(!colaAuxiliar.esVacia()){
+            nuevaCola.poner(colaAuxiliar.obtenerFrente());
+            colaAuxiliar.sacar();
         }
         return nuevaCola;
     }
